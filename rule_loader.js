3462a3447f9ac10bed3c1c397bbb29782a20e80b
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-12 11:56:53 -0700

    started building a rule loader

---
+++ rule_loader.js
@@ -0,0 +1,11 @@
+var boardO = boardO || {};
+
+add_element = function(){
+    console.log("I still need to actually make this");
+}
+
+inputs = document.getElementsByClassName("rule-input");
+//console.log(inputs);
+for(let i = 0; i<inputs.length; i++){
+//        console.log(inputs[i].id);
+}
