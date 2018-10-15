226afe93437ed110b4c776f4553cabf31d466c07
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-15 12:00:10 -0700

    found check boxes and generated list

--- rule_loader.js
+++ rule_loader.js
@@ -2,15 +2,32 @@ var boardO = boardO || {};
 
 
 
-inputs = document.getElementsByClassName("rule-input");
-//console.log(inputs);
-for(let i = 0; i<inputs.length; i++){
-//        console.log(inputs[i].id);
-}
+//inputs = document.getElementsByClassName("rule-input");
+////console.log(inputs);
+//for(let i = 0; i<inputs.length; i++){
+////        console.log(inputs[i].id);
+//}
 
 
 update_rules = function(){
-    
+    let elements = document.getElementsByClassName("changes");
+    for(let i = 0; i < elements.length; i++){
+        let current = elements[i]
+        let conditions = current.getElementsByClassName("condition");
+        for(let condition_loop = 0; condition_loop < conditions.length; condition_loop++){
+            let condition = conditions[condition_loop];
+            let checks = condition.getElementsByClassName("checkbox");
+            let my_rule = []
+            for(let check_loop = 0; check_loop < checks.length; check_loop++){
+                let check = checks[check_loop];
+                if(check.checked){
+                    my_rule.push(Number(check.value));
+                }
+            }
+            console.log(i,condition_loop,my_rule);
+        }
+        
+    }
 }
 
 add_element = function(){
