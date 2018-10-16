47c0835c54666b8923e4a2569ecd337144995561
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-15 17:05:43 -0700

    tried to update html, currently not working

--- rule_loader.js
+++ rule_loader.js
@@ -61,6 +61,73 @@ update_rules = function(){
     }
 }
 
+
+updateHTML = function(){
+    htmlString = `<ul>
+`
+    for(let rule_loop = 0; rule_loop<globalRules.length; rule_loop++){
+        htmlString += `<li>
+
+`
+        htmlString += `
+<label>cell color: </label>
+<div class="rule-color" style="background: `+globalRules.colors[rule_loop]+`"></div>
+<ul id="changes">
+
+`
+
+        rule = globalRules[rule_loop];
+        //        console.log(rule);
+
+        for(let change_loop = 0; change_loop<rule.length; change_loop++){
+            htmlString += `<li>
+<hr>
+</li>
+`
+            htmlString += `<li class="change">
+`
+            htmlString += `<label class="changeto-label">change to: </label>
+`
+            htmlString += `<div class="changeto-color" style="background: `+globalRules.colors[change_loop]+`"></div>
+<ul class="conditions">
+`
+            change = rule[change_loop];
+            //            console.log(change)
+            for(let x = 0; x < globalRules.length; x++){
+                htmlString+=`<li class="condition">
+<div class="conditions-color" style="background: `+globalRules.colors[x]+`"></div>
+<div class="selector">
+<p class="selector_text">0 1 2 3 4 5 6 7 8</p>`;
+                for(let y = 0; y < 9; y++){
+                    htmlString+=`<input type="checkbox" value="`+y+`" class="checkbox"`
+                    if(x in change.conditions){
+                        if(change.conditions[x].includes(y)){
+                            htmlString+=` checked `
+                        }
+                        
+                    }
+                    htmlString+=`>`
+
+                }
+                htmlString+=`</div>`;
+            }
+            //            console.log(change);
+
+            htmlString+=`</ul></li>`
+        }
+        htmlString += `</ul>
+</li>
+`
+
+    }
+    htmlString+=`</ul>
+`             
+    console.log(htmlString)
+    creator = document.getElementById("rule-creator");
+    creator.innerHTML = htmlString;
+
+}
+
 add_element = function(){
     globalRules[globalRules.length] = []
 
@@ -71,4 +138,6 @@ add_element = function(){
     globalRules[globalRules.length].push({change_to:globalRules.length, conditions:{}});
 
     globalRules.length++;
+    updateHTML();
+    set_listeners()
 }
