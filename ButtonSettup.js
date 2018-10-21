2d0348a53398b1776b3d0f00b8f903a039ca8963
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-20 20:58:54 -0700

    added rule presets

--- ButtonSettup.js
+++ ButtonSettup.js
@@ -7,6 +7,7 @@ const delayLabel = document.getElementById("delay-label");
 const delayRange = document.getElementById("delay-range");
 const stepButton = document.getElementById("step-button");
 const randomizeRulesButton = document.getElementById("randomize-rules");
+const rulePreset = document.getElementById("rule-preset");
 
 let mouseOnRange = false;
 
@@ -35,4 +36,36 @@ stepButton.onclick = (e) => {
 
 randomizeRulesButton.onclick = (e) => {
     randomizeRules();
+}
+
+rulePreset.onchange = (e) => {
+    if(rulePreset.selectedIndex!=0){
+        const presets = {
+            "Game of Life":{
+                0:[{changeTo:0, conditions:{}},{changeTo: 1, conditions: {1:[3]}}],
+                1:[{changeTo: 0, conditions: {1:[0,1,4,5,6,7,8]}},{changeTo:1,conditions:{}}],
+                length: 2,
+                colors:["white","black","blue","Red"]
+            },
+            "High Life":{
+                0:[{changeTo:0, conditions:{}},{changeTo: 1, conditions: {1:[3,6]}}],
+                1:[{changeTo: 0, conditions: {1:[0,1,4,5,6,7,8]}},{changeTo:1,conditions:{}}],
+                length: 2,
+                colors:["white","black","blue","Red"]
+            },
+
+        };
+
+
+        globalRules = (presets[rulePreset.options[rulePreset.selectedIndex].value]);
+        if(globalRules.length === boardO.rules.length){
+            boardO.rules = globalRules;
+        }
+        else{
+            boardO.reset(globalRules);
+        }
+
+        updateHTML();
+    }
+
 }
